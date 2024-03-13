d899a667a6dbf102790b5edfd29db5faf7949b86
roxasender <pn0165652@gmail.com>
2024-03-13 10:33:37 +0700

    adding nav links

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import { Nav } from "./components"
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <main className="relative">
+      <Nav/>
+      <section>
+        
+      </section>
+    </main>
   )
 }
---
+++ src/components/Nav.jsx
@@ -0,0 +1,31 @@
+import { navLinks } from "../constains";
+
+export default function Nav() {
+  return (
+    <header className="padding-x py-8 absolute z-10 w-full">
+      <nav className="flex justify-between items-center max-container">
+        {/* Logo */}
+        <a href="/">
+          <img src="" alt="rsptshirt-logo" width={129} height={29} />
+        </a>
+        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <a
+                href={item.href}
+                className="font-montserrat leading-normal text-lg text-slate-gray"
+              >
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className="flex gap-2 text-lg leading-normal font-medium font-montserrat max-lg:hidden wide:mr-24">
+          <a href="/">Sign in</a>
+          <span>/</span>
+          <a href="/">Explore now</a>
+        </div>
+      </nav>
+    </header>
+  );
+}
